2fb6cc4a9833edfba10d8debb05a4222af68c46e
JSP <pjs12041204@gmail.com>
2022-04-19 19:30:26 +0900

    balancegame 테스트 코드 작성

--- src/main/java/hsu/bee/petra/schedule/controller/TravelCodeController.java
+++ src/main/java/hsu/bee/petra/schedule/controller/TravelCodeController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-
-
 @RequiredArgsConstructor
 @RestController
 public class TravelCodeController {
--- src/main/java/hsu/bee/petra/schedule/dto/BalanceGameAnswerDto.java
+++ src/main/java/hsu/bee/petra/schedule/dto/BalanceGameAnswerDto.java
@@ -3,8 +3,10 @@ package hsu.bee.petra.schedule.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class BalanceGameAnswerDto {
 
